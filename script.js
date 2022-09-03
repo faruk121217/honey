a24f09e7bc2c9486cc2a8838507bee704cfb2e59
faruk121217 <66951467+faruk121217@users.noreply.github.com>
2022-09-03 22:58:56 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "toma" && b=="619619")
+  	{
+  	    window.location.href="open.html";
+  	}
+  	else
+  	{
+  		alert("Sorry! You haven't no right to enter here");
+  		window.location.href="index.html";
+  	}
+  	
