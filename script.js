ca0e5222727e7ab9b615fc3307ed28a10d60d815
faruk121217 <66951467+faruk121217@users.noreply.github.com>
2024-06-30 12:06:18 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var a=prompt("Enter UserName");
  var b=prompt("Enter password");
-  	if(a== "toma" && b=="620620")
+  	if(a== "honey" && b=="608608")
   	{
   	    window.location.href="open.html";
   	}
