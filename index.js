a17409aa257ef4be98b76ac4fbf27e6cfe0b5b64
faruk121217 <66951467+faruk121217@users.noreply.github.com>
2024-06-30 12:20:31 +0600

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'July, 12, 2023',
+  birthdate: 'July, 12, 2024',
   name: 'Honey'
 };
 
